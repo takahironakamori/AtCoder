0ff93191d9c6b1bc200522bc3f92afc0b89a53d9
Takahiro Nakamori <git@takahironakamori.jp>
2020-04-02 00:18:59 +0900

    【A】AC【B】AC【C】AC【D】-

---
+++ ABC/ABC071/C/main.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int N;
+  cin >> N;
+
+  map<int, int, greater<int>> A;
+  for (int i = 0; i < N; i++) {
+    int a;
+    cin >> a;
+    A[a]++;
+  }
+
+  int sideA = 0;
+  int sideB = 0;
+
+  for (auto item: A) {
+    if (4 <= item.second) {
+      if (sideA == 0) {
+        sideA = item.first;
+      }
+      if (sideB == 0) {
+        sideB = item.first;
+        break;
+      }
+    } else if (2 <= item.second) {
+      if (sideA == 0) {
+        sideA = item.first;
+      } else if (sideB == 0) {
+        sideB = item.first;
+        break;
+      }
+    }
+  }
+
+  long long answer = (long long) sideA * sideB;
+
+  cout << answer << endl;
+
+  return 0;
+
+}
