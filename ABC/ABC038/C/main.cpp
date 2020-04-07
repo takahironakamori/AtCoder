65febe64d070cb876b656d2c5bc44f4ac997703e
Takahiro Nakamori <git@takahironakamori.jp>
2020-04-07 21:28:53 +0900

    【A】AC【B】AC【C】AC【D】-

---
+++ ABC/ABC038/C/main.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int N;
+  cin >> N;
+
+  vector<int> a(N);
+  for (int i = 0; i < N; i++) {
+    cin >> a[i];
+  }
+
+  long long answer = N;
+  int current = 0;
+
+  for (int i = 1; i < N; i++) {
+    if (a[i - 1] < a[i]) {
+      answer += i - current;
+    } else {
+      current = i;
+    }
+  }
+
+  cout << answer << endl;
+
+  return 0;
+
+}
