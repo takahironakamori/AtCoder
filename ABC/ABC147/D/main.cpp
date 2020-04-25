3629ab138df6504d1cc2f625f04a252c078eaaf0
Takahiro Nakamori <git@takahironakamori.jp>
2020-04-25 23:21:26 +0900

    【A】AC【B】AC【C】AC【D】AC【E】-【F】-

---
+++ ABC/ABC147/D/main.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mod = 1000000007;
+
+int main() {
+
+  int N;
+  cin >> N;
+
+  vector<long long> A(N);
+  for (int i = 0; i < N; i++) {
+    cin >> A[i];
+  }
+
+  long long answer = 0;
+
+  // 桁ごとに考える
+  // A_i の最大は、2進数で60桁
+  for (int i = 0; i < 60; i++) {
+
+    // すべてのAのi桁目の1の個数を数える
+    long long count = 0;
+    for (int j = 0; j < N; j++) {
+      if (A[j] >> i & 1) {
+        ++count;
+      }
+    }
+
+    // 1の個数 × 0の個数 × 2の桁乗
+    // まずは、1の個数 × 0の個数 を計算しておく
+    long long v = count * (N - count) % mod;
+
+    // 2の桁乗を計算する
+    for (int j = 0; j < i; j++) {
+      v = v * 2 % mod;
+    }
+
+    answer += v;
+    answer %= mod;
+
+  }
+
+  cout << answer << endl;
+
+  return 0;
+
+}
